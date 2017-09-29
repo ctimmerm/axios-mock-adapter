6523305feed29d0668c598408c979fbd13e8809e
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2017-09-29 10:53:06 +0200

    TypeScript definitions (#76)
    * add basic typings
    
    * improve typings
    
    * adjust typings for DefinitelyTyped

---
+++ types/index.d.ts
@@ -0,0 +1,45 @@
+import { AxiosAdapter, AxiosInstance, AxiosRequestConfig } from "axios";
+
+type CallbackResponseSpecFunc = (config: AxiosRequestConfig) => any[] | Promise<any[]>;
+
+type ResponseSpecFunc = (statusOrCallback: number | CallbackResponseSpecFunc, data?: any, headers?: any) => MockAdapter;
+
+interface RequestHandler {
+  reply: ResponseSpecFunc;
+  replyOnce: ResponseSpecFunc;
+
+  passThrough(): void;
+  networkError(): void;
+  timeout(): void;
+}
+
+interface MockAdapterOptions {
+  delayResponse?: number;
+}
+
+interface RequestDataMatcher {
+  [index: string]: any;
+  params?: {
+    [index: string]: any,
+  };
+}
+
+type RequestMatcherFunc = (matcher?: string | RegExp, body?: string | RequestDataMatcher) => RequestHandler;
+
+declare class MockAdapter {
+  constructor(axiosInstance: AxiosInstance, options?: MockAdapterOptions);
+
+  adapter(): AxiosAdapter;
+  reset(): void;
+  restore(): void;
+
+  onGet: RequestMatcherFunc;
+  onPost: RequestMatcherFunc;
+  onPut: RequestMatcherFunc;
+  onHead: RequestMatcherFunc;
+  onDelete: RequestMatcherFunc;
+  onPatch: RequestMatcherFunc;
+  onAny: RequestMatcherFunc;
+}
+
+export = MockAdapter;
